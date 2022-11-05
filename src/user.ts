622e58fcc7a71fe50a17245bb495fd49abb45c39
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-11-05 03:46:06 -0400

    docs: add user model documentation

--- src/user.ts
+++ src/user.ts
@@ -1,16 +1,56 @@
 import { Schema, Document as MongoDocument, model } from 'mongoose'
 
+/**
+ * @protected
+ */
 const schemaName = 'User'
 
+/**
+ * User model parameters
+ * @see [Mongoose Schema Types](https://mongoosejs.com/docs/schematypes.html)
+ */
 interface IUser extends MongoDocument {
+    /**
+     * The real and public User Name
+     * @public
+     */
     name: string
-    area: string
+    /**
+     * The job position of User
+     * @public
+     */
+    position: string
+    /**
+     * The registration email
+     * > NOTE: No expose this parametter to public
+     * @public
+     */
     mail: string
+    /**
+     * The registration password
+     * > NOTE: No expose this parametter to public
+     * @public
+     */
+    password: string
+    /**
+     * The biography of User
+     * @public
+     */
     description: string
+    /**
+     * The presentation video of User
+     * @public
+     */
     presentationVideo: string
 }
 
+/**
+ * Schema of Mongoose
+ */
 const UserSchema = new Schema<IUser>()
+/**
+ * The Mongoose model of User
+ */
 const User = model<IUser>(schemaName, UserSchema)
 
 export { schemaName, IUser, UserSchema, User }
